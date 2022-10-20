2178db91ef2a713cc06f97647492f28f6a2dd755
Gabriel Cardoso <41308499+gcard4@users.noreply.github.com>
2022-10-20 09:13:50 -0300

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,49 @@
+// - 1 - dar um jeito de pegar os elementos que representam as abas no HTML
+
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach((aba) => {
+
+// - 2 - Método para identificar o clique no elemento da aba
+
+  aba.addEventListener("click", function () {
+    if (aba.classList.contains("selecionado")) {
+      return;
+    }
+
+selecionarAba(aba)
+
+mostrarInformacoesDaAba(aba)
+
+  });
+});
+
+function selecionarAba(aba) {
+
+//  - 3 - quando o usuário clicar, desmarcar a aba selecionada
+
+  const abaSelecionada = document.querySelector(".aba.selecionado");
+  abaSelecionada.classList.remove("selecionado");
+
+// - 4 - marcar a aba clicada como selecionado
+
+  aba.classList.add("selecionado");
+}
+
+function mostrarInformacoesDaAba (aba) {
+
+  // - 5 - esconder o conteúdo anterior
+
+  const informacaoSelecionada = document.querySelector(
+    ".informacao.selecionado"
+  );
+  informacaoSelecionada.classList.remove("selecionado");
+
+// - 6 - mostrar o conteúdo da aba selecionada
+
+  const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+
+  const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
+  informacaoASerMostrada.classList.add ("selecionado")
+
+}
